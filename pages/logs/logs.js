aff22fe4e7e1fa05e0056f359ab1b5cc3d925be5
Mr吴 <1970375548@qq.com>
2019-05-28 16:04:26 +0800

    add fuction day caculate

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,48 +1,52 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs:[],
+    logs: [],
     modalHidden: true,
     toastHidden: true,
+    workTimePercent: '',
+    showStudyTime: '',
     bgimage: '../../image/bg.jpg'
   },
-  onShow: function() {
+  onShow: function () {
+    var time = calculateTime();
+    var h = parseInt(time / 3600 / 1000);
+    var m = parseInt(time % (1000 * 3600) / 60 / 1000);
+    var s = parseInt(time % (1000 * 3600) % (60 * 1000) / 1000);
+    this.setData({
+      workTimePercent: time / 3600 / 1000 * 100 + '%',
+      showStudyTime: h + '时' + m + '分' + s + '秒'
+    });
     wx.setNavigationBarTitle({
       title: '任务记录'
-    })
+    });
     this.getLogs();
-    // 背景图片切换
-    if (wx.getStorageSync('bgimage'))
-     { this.setData({ bgimage: wx.getStorageSync('bgimage') }) }
-     else
-     {
-      this.setData({ bgimage: '../../image/bg.jpg' })
-     }
+    if (wx.getStorageSync('bgimage')) { this.setData({ bgimage: wx.getStorageSync('bgimage') }) }
   },
-  set: function() {
-
+  set: function () {
   },
-  getLogs: function() {
-    var logs = wx.getStorageSync('logs')
-    logs.forEach(function(item, index, arry) {
+  getLogs: function () {
+    let logs = wx.getStorageSync('logs')
+    logs.forEach(function (item, index, arry) {
       item.startTime = new Date(item.startTime).toLocaleString()
     })
     this.setData({
       logs: logs
     })
   },
-  onLoad: function() {},
-  switchModal: function() {
+  onLoad: function () {
+  },
+  switchModal: function () {
     this.setData({
       modalHidden: !this.data.modalHidden
     })
   },
-  hideToast: function() {
+  hideToast: function () {
     this.setData({
       toastHidden: true
     })
   },
-  clearLog: function(e) {
+  clearLog: function (e) {
     wx.setStorageSync('logs', [])
     this.switchModal()
     this.setData({
@@ -51,3 +55,32 @@ Page({
     this.getLogs()
   }
 })
+
+function calculateTime() {
+  var time = 0;
+  var timestamp = Date.parse(new Date());
+  var date = new Date(timestamp);
+  var Y = date.getFullYear();
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
+  var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  let logs = wx.getStorageSync('logs');
+  var endTimeTemp;
+  logs.forEach(function (item, index, arry) {
+    if (item.name == '工作') {
+      if (item.action == '开始') {
+        var startTime = item.startTime;
+        var temp = new Date(startTime);
+        var tempY = temp.getFullYear();
+        var tempM = (temp.getMonth() + 1 < 10 ? '0' + (temp.getMonth() + 1) : temp.getMonth() + 1);
+        var tempD = temp.getDate() < 10 ? '0' + temp.getDate() : temp.getDate();
+        if (tempD == D && tempM == M && tempY == Y) {
+          time += endTimeTemp - startTime;
+        }
+      } else {
+        endTimeTemp = item.startTime;
+      }
+    }
+  });
+  console.log(time);
+  return time;
+}
